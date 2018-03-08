571c07021aacd8dda1fef9e20756a1d9b95f0c80
Eaug2 <erik.augenzucker@gmail.com>
2018-03-08 18:53:16 -0500

    spacing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var wins = 0;
     var looses = 0;
     var userTotal = 0;
 
     //targetNumb between 19-120
-    var targetNumb = Math.floor(Math.random()*101+19);
+    var targetNumb = Math.floor(Math.random() * 101 + 19);
     $("#goal-number").text("Number to Guess: " + targetNumb);
     console.log(targetNumb);
     console.log("-----");
-    
+
 
     // crystal value between 1-12
-    
-    var crystal1 = Math.floor(Math.random()*11+1);
-    var crystal2 = Math.floor(Math.random()*11+1);
-    var crystal3 = Math.floor(Math.random()*11+1);
-    var crystal4 = Math.floor(Math.random()*11+1);
-    
+
+    var crystal1 = Math.floor(Math.random() * 11 + 1);
+    var crystal2 = Math.floor(Math.random() * 11 + 1);
+    var crystal3 = Math.floor(Math.random() * 11 + 1);
+    var crystal4 = Math.floor(Math.random() * 11 + 1);
+
 
     $("#wins").text("Wins: " + wins);
     $("#looses").text("Looses: " + looses);
 
-    function reset(){
-         targetNumb = Math.floor(Math.random()*101+19);
+    function reset() {
+        targetNumb = Math.floor(Math.random() * 101 + 19);
         $("#goal-number").text("Number to Guess: " + targetNumb);
         console.log(targetNumb);
         console.log("-----");
-         crystal1 = Math.floor(Math.random()*11+1);
-         crystal2 = Math.floor(Math.random()*11+1);
-         crystal3 = Math.floor(Math.random()*11+1);
-         crystal4 = Math.floor(Math.random()*11+1);
+        crystal1 = Math.floor(Math.random() * 11 + 1);
+        crystal2 = Math.floor(Math.random() * 11 + 1);
+        crystal3 = Math.floor(Math.random() * 11 + 1);
+        crystal4 = Math.floor(Math.random() * 11 + 1);
         userTotal = 0;
         $("#finalTotal").text(userTotal);
     };
 
-//first crystal
-    $("#one").on("click", function(){
+    //first crystal
+    $("#one").on("click", function () {
         userTotal = userTotal + crystal1;
         $("#finalTotal").text(userTotal);
 
-        if(userTotal === targetNumb){
+        if (userTotal === targetNumb) {
             alert("You Win!");
             wins++;
             $("#wins").text("Wins: " + wins);
             reset();
         }
-        else if (userTotal > targetNumb){
+        else if (userTotal > targetNumb) {
             alert("You Lose!");
             looses++;
             $("#looses").text("Looses: " + looses);
@@ -53,18 +53,18 @@ $(document).ready(function(){
         }
     });
 
-//second crystal
-    $("#two").on("click", function(){
+    //second crystal
+    $("#two").on("click", function () {
         userTotal = userTotal + crystal2;
         $("#finalTotal").text(userTotal);
 
-        if(userTotal === targetNumb){
+        if (userTotal === targetNumb) {
             alert("You Win!");
             wins++;
             $("#wins").text("Wins: " + wins);
             reset();
         }
-        else if (userTotal > targetNumb){
+        else if (userTotal > targetNumb) {
             alert("You Lose!");
             looses++;
             $("#looses").text("Looses: " + looses);
@@ -72,18 +72,18 @@ $(document).ready(function(){
         }
     });
 
-// third crystal
-    $("#three").on("click", function(){
+    // third crystal
+    $("#three").on("click", function () {
         userTotal = userTotal + crystal3;
         $("#finalTotal").text(userTotal);
 
-        if(userTotal === targetNumb){
+        if (userTotal === targetNumb) {
             alert("You Win!");
             wins++;
             $("#wins").text("Wins: " + wins);
             reset();
         }
-        else if (userTotal > targetNumb){
+        else if (userTotal > targetNumb) {
             alert("You Lose!");
             looses++;
             $("#looses").text("Looses: " + looses);
@@ -91,18 +91,18 @@ $(document).ready(function(){
         }
     });
 
-// forth crystal
-    $("#four").on("click", function(){
+    // forth crystal
+    $("#four").on("click", function () {
         userTotal = userTotal + crystal4;
         $("#finalTotal").text(userTotal);
 
-        if(userTotal === targetNumb){
+        if (userTotal === targetNumb) {
             alert("You Win!");
             wins++;
             $("#wins").text("Wins: " + wins);
             reset();
         }
-        else if (userTotal > targetNumb){
+        else if (userTotal > targetNumb) {
             alert("You Lose!");
             looses++;
             $("#looses").text("Looses: " + looses);
@@ -110,6 +110,6 @@ $(document).ready(function(){
         }
     });
 
-    
+
 
 });
